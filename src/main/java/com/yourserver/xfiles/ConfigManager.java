bf5675fb58179bfa6335a3739932b7968f653706
gordey9992 <146545641+gordey9992@users.noreply.github.com>
2025-10-20 15:28:56 +0300

    Update ConfigManager.java

--- src/main/java/com/yourserver/xfiles/ConfigManager.java
+++ src/main/java/com/yourserver/xfiles/ConfigManager.java
@@ -1,11 +1,11 @@
 package com.yourserver.xfiles;
 
+import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.List;
 
 public class ConfigManager {
